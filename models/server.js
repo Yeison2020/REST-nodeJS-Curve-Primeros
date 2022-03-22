24b447c00dff731dbb8874987c2e5f8626c2fabe
Yeison <enmanuelquiterios@gmail.com>
2022-03-22 10:23:26 -0400

    Creating my server class

---
+++ models/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+require("dotenv").config();
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    // Here is How I call my methods when the constructor gets call at the end
+    this.routes();
+  }
+  routes() {
+    this.app.get("/", function (req, res) {
+      res.send("Hello World");
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Server Running on port:", this.port);
+    });
+  }
+}
+
+module.export = Server;
